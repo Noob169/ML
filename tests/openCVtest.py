d2c4d0900fb50b0f77c1896882912010e0053934
Noob169 <boney0310@gmail.com>
2022-06-26 01:40:46 +0530

    test file for openCV

---
+++ tests/openCVtest.py
@@ -0,0 +1,20 @@
+import cv2
+
+# main code
+if __name__ == "__main__" :
+ 
+    # mentioning absolute path of the image
+    img_path = "C:\\Users\\User\\Desktop\\photo_new and improved.jpeg"
+ 
+    # read/load an image
+    image = cv2.imread(img_path)
+ 
+    # show the input image on the newly created window
+    cv2.imshow('input image',image)
+ 
+    # convert image from BGR color space to GRAY color space
+    convert_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY )
+ 
+    # show the output image on the newly created window
+    cv2.imshow('output image',convert_image)
+    cv2.waitKey(0)
